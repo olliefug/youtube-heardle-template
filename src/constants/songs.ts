811c4e084314b2e0bfb77f75591390b1058028d7
olliefug <olliegunnargaming@gmail.com>
2026-01-09 05:45:57 +0000

    Update songs.ts
    update to AWTY

--- src/constants/songs.ts
+++ src/constants/songs.ts
@@ -24,4 +24,75 @@ export const songs = [
     name: "Wake Up!",
     youtubeId: "sVHr0bHH3LE",
   },
+  {
+    artist: "James Marriott",
+    name: "Gold",
+    youtubeId: "w1agTgiJOgs",
+  },
+  {
+    artist: "James Marriott",
+    name: "Sleeping On Trains",
+    youtubeId: "a3Ij26WwI1E",
+  },
+  {
+    artist: "James Marriott",
+    name: "Where Has Everyone Gone?",
+    youtubeId: "o8OUMq37nBI",
+  },
+  {
+    artist: "James Marriott",
+    name: "Grapes",
+    youtubeId: "-uk3GwgNc3o",
+  },
+  {
+    artist: "James Marriott",
+    name: "So Long",
+    youtubeId: "Dpqs8ehkwFc",
+  },
+  {
+    artist: "James Marriott",
+    name: "Romanticise This",
+    youtubeId: "MevyckBF6FE",
+  },
+  {
+    artist: "James Marriott",
+    name: "Don't Blame Me",
+    youtubeId: "9GZ6hs0V0ME",
+  },
+  {
+    artist: "James Marriott",
+    name: "You Are Here",
+    youtubeId: "-ovH-d8a1t0",
+  },
+  {
+    artist: "James Marriott",
+    name: "Going Postal At The Party",
+    youtubeId: "en8SqY3ENdA",
+  },
+  {
+    artist: "James Marriott",
+    name: "Over My Head",
+    youtubeId: "X1PILrtWohQ",
+  },
+  {
+    artist: "James Marriott",
+    name: "In Between",
+    youtubeId: "T6WMaEOplrw",
+  },
+  {
+    artist: "James Marriott",
+    name: "The Other Side",
+    youtubeId: "Kd4DU_H4s-M",
+  },
+  {
+    artist: "James Marriott",
+    name: "White Noise",
+    youtubeId: "NayhVDpYZDg",
+  },
+  {
+    artist: "James Marriott",
+    name: "Denial",
+    youtubeId: "IanTuGYyznM",
+  },
+  
 ];
