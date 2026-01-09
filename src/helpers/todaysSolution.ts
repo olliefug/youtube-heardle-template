774f095e36dce152b0f01faa0bdeed268c73534f
olliefug <olliegunnargaming@gmail.com>
2026-01-09 04:59:00 +0000

    Update todaysSolution.ts
    change to unlimited mode

--- src/helpers/todaysSolution.ts
+++ src/helpers/todaysSolution.ts
@@ -1,12 +1,3 @@
-import { songs } from "../constants";
+import { songs } from "../constants/songs";  // Adjust path if it's just "../constants" – match your original import
 
-// const epochMs = new Date(2022, 3, 10).valueOf();
-// const now = Date.now();
-// const index = Math.floor((now - epochMs) / msInDay);
-
-const msInDay = 86400000;
-const startDate = new Date('4/15/2022');
-const todaysDate = new Date();
-const index = Math.floor((todaysDate.getTime() - startDate.getTime() )/msInDay)
-
-export const todaysSolution = songs[index % songs.length];
+export const todaysSolution = songs[Math.floor(Math.random() * songs.length)];
