10017348d2160681f790e19d640e1126b856cd8f
olliefug <olliegunnargaming@gmail.com>
2026-01-09 05:27:47 +0000

    Update songs.ts
    3 new songs

--- src/constants/songs.ts
+++ src/constants/songs.ts
@@ -4,4 +4,19 @@ export const songs = [
     name: "Slow Down",
     youtubeId: "n20BNnJA7f4",
   },
+  {
+    artist: "James Marriott",
+    name: "Him",
+    youtubeId: "SY6nga5gOeY",
+  },
+  {
+    artist: "James Marriott",
+    name: "New Face",
+    youtubeId: "Vc574tlH4_c",
+  },
+  {
+    artist: "James Marriott",
+    name: "New Face",
+    youtubeId: "arktWVQkQws",
+  },
 ];
